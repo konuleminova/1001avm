9902a363a95f8b7e01047ab6fc86197a4acd8f22
konul <konul.eminova39@gmail.com>
2019-07-18 17:04:35 +0400

    initial menu

---
+++ lib/page/product_detail.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ProductDetailState();
+  }
+
+}
+
+class ProductDetailState extends State<ProductDetail> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+}
