3b5bbd13f9f92a9105bf5e8e790084d819c85626
konul <konul.eminova39@gmail.com>
2019-07-22 15:49:59 +0400

    initial menu

--- lib/page/product_detail.dart
+++ lib/page/product_detail.dart
@@ -18,9 +18,12 @@ class ProductDetail extends StatefulWidget {
 class ProductDetailState extends State<ProductDetail> {
   Product product;
 
+  var width;
+
   @override
   Widget build(BuildContext context) {
     product = widget.product;
+    width = MediaQuery.of(context).size.width;
     // TODO: implement build
     return Scaffold(
         backgroundColor: Colors.white,
@@ -78,164 +81,205 @@ class ProductDetailState extends State<ProductDetail> {
               ),
             ),
             Container(
-                height: 550,
-                margin: EdgeInsets.all(16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      product.title,
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: fixedColor.withOpacity(0.8)),
-                    ),
-                    rowStar(),
-                    RichText(
-                      text: new TextSpan(
-                        // Note: Styles for TextSpans must be explicitly defined.
-                        // Child text spans will inherit styles from parent
-                        style: new TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.black,
-                        ),
-                        children: <TextSpan>[
-                          new TextSpan(
-                              text: 'Reference: ',
-                              style:
-                                  new TextStyle(fontWeight: FontWeight.bold)),
-                          new TextSpan(text: 'Hello'),
-                        ],
+              height: 550,
+              margin: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    product.title,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: fixedColor.withOpacity(0.8)),
+                  ),
+                  rowStar(),
+                  RichText(
+                    text: new TextSpan(
+                      // Note: Styles for TextSpans must be explicitly defined.
+                      // Child text spans will inherit styles from parent
+                      style: new TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
                       ),
+                      children: <TextSpan>[
+                        new TextSpan(
+                            text: 'Reference: ',
+                            style: new TextStyle(fontWeight: FontWeight.bold)),
+                        new TextSpan(text: 'Hello'),
+                      ],
                     ),
-                    RichText(
-                      text: new TextSpan(
-                        // Note: Styles for TextSpans must be explicitly defined.
-                        // Child text spans will inherit styles from parent
-                        style: new TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.black,
-                        ),
-                        children: <TextSpan>[
-                          new TextSpan(
-                              text: 'Condition: ',
-                              style:
-                                  new TextStyle(fontWeight: FontWeight.bold)),
-                          new TextSpan(text: 'New product'),
-                        ],
+                  ),
+                  RichText(
+                    text: new TextSpan(
+                      // Note: Styles for TextSpans must be explicitly defined.
+                      // Child text spans will inherit styles from parent
+                      style: new TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
                       ),
+                      children: <TextSpan>[
+                        new TextSpan(
+                            text: 'Condition: ',
+                            style: new TextStyle(fontWeight: FontWeight.bold)),
+                        new TextSpan(text: 'New product'),
+                      ],
                     ),
-                    RichText(
-                      text: new TextSpan(
-                        // Note: Styles for TextSpans must be explicitly defined.
-                        // Child text spans will inherit styles from parent
-                        style: new TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.black,
-                        ),
-                        children: <TextSpan>[
-                          new TextSpan(
-                              text: '10 ',
-                              style:
-                                  new TextStyle(fontWeight: FontWeight.bold)),
-                          new TextSpan(text: 'Items'),
-                        ],
+                  ),
+                  RichText(
+                    text: new TextSpan(
+                      // Note: Styles for TextSpans must be explicitly defined.
+                      // Child text spans will inherit styles from parent
+                      style: new TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
                       ),
+                      children: <TextSpan>[
+                        new TextSpan(
+                            text: '10 ',
+                            style: new TextStyle(fontWeight: FontWeight.bold)),
+                        new TextSpan(text: 'Items'),
+                      ],
                     ),
-                    Divider(),
-                    Text(
-                      "1234567879 AZN tax incl.",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 21,
-                          color: fixedColor.withOpacity(0.8)),
+                  ),
+                  Divider(),
+                  Text(
+                    "1234567879 AZN tax incl.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 21,
+                        color: fixedColor.withOpacity(0.8)),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "QUANTITY",
+                        style: TextStyle(
+                            color: fixedColor.withOpacity(0.6),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Container(
+                        child: Text("1"),
+                        padding: EdgeInsets.all(5),
+                        width: 50,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: fixedColor.withOpacity(0.2))),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  RaisedButton(
+                    onPressed: null,
+                    child: Padding(
+                      child: Text("Add to Cart"),
+                      padding: EdgeInsets.all(12.0),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0)),
+                    color: Colors.orange,
+                    disabledColor: Colors.orange,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        child: Icon(Icons.mail_outline, color: fixedColor),
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: fixedColor.withOpacity(0.2))),
+                      ),
+                      Container(
+                        child: Icon(Icons.share, color: fixedColor),
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: fixedColor.withOpacity(0.2))),
+                      ),
+                      Container(
+                        child: Icon(Icons.print, color: fixedColor),
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: fixedColor.withOpacity(0.2))),
+                      ),
+                      Container(
+                        child: Icon(Icons.favorite, color: fixedColor),
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: fixedColor.withOpacity(0.2))),
+                      ),
+                      Container(
+                        child: Icon(Icons.redo, color: fixedColor),
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: fixedColor.withOpacity(0.2))),
+                      ),
+                      SizedBox(
+                        width: 100,
+                      )
+                    ],
+                  ),
+                  Divider(),
+                  Text(
+                    'Descrioption Lorem ipsum.LED 43"; 9.6 kg; 97.5 x 85.2 x 57.1 Səs gücü 20w; Stereo - var; Wi-Fi - var',
+                    style: TextStyle(color: fixedColor),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(12.0),
+                    width: width,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: fixedColor.withOpacity(0.2))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
+                        Icon(
+                          Icons.question_answer,
+                          size: 15.0,
+                          color: fixedColor.withOpacity(0.8),
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                        ),
                         Text(
-                          "QUANTITY",
-                          style: TextStyle(
-                              color: fixedColor.withOpacity(0.6),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        ),
-                        SizedBox(
-                          width: 20.0,
-                        ),
-                        Container(
-                          child: Text("1"),
-                          padding: EdgeInsets.all(5),
-                          width: 50,
-                          height: 30,
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: fixedColor.withOpacity(0.2))),
+                          "REVIEWS",
+                          style: TextStyle(color: fixedColor.withOpacity(0.8)),
                         )
                       ],
                     ),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    RaisedButton(
-                      onPressed: null,
-                      child: Padding(
-                        child: Text("Add to Cart"),
-                        padding: EdgeInsets.all(12.0),
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(40.0)),
-                      color: Colors.orange,
-                      disabledColor: Colors.orange,
-                    ),Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Container(
-                          child: Icon(Icons.mail_outline, color: fixedColor),
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: fixedColor.withOpacity(0.2))),
-                        ),
-                        Container(
-                          child: Icon(Icons.share, color: fixedColor),
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: fixedColor.withOpacity(0.2))),
-                        ),
-                        Container(
-                          child: Icon(Icons.print, color: fixedColor),
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: fixedColor.withOpacity(0.2))),
-                        ),
-                        Container(
-                          child: Icon(Icons.favorite, color: fixedColor),
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: fixedColor.withOpacity(0.2))),
-                        ),
-                        Container(
-                          child: Icon(Icons.redo, color: fixedColor),
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: fixedColor.withOpacity(0.2))),
-                        ),
-                        SizedBox(
-                          width: 100,
-                        )
-                      ],
-                    ),
-                    Divider(),
-                    Text(
-                      'Descrioption Lorem ipsum.LED 43"; 9.6 kg; 97.5 x 85.2 x 57.1 Səs gücü 20w; Stereo - var; Wi-Fi - var',
-                      style: TextStyle(color: fixedColor),
-                    )
-                  ],
-                ),),
-
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(12.0),
+                      width: width,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(color: fixedColor.withOpacity(0.2))),
+                      child: Text(
+                        "CUSTOM",
+                        style: TextStyle(color: fixedColor.withOpacity(0.8)),
+                      ))
+                ],
+              ),
+            ),
           ],
         ));
   }
